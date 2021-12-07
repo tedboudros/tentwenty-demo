4150126936aabc9cbf531786afc056899af3129e
Ted Boudros <theodore.boudros.design@gmail.com>
2021-12-08 01:28:43 +0200

    [feat] Add landing background and lecticals

--- with-cra/src/view/sections/landing/index.js
+++ with-cra/src/view/sections/landing/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import landingBg from "assets/images/landing-bg.png";
+
 const Landing = () => {
-  return <div></div>;
+  return (
+    <div className="landing">
+      <div className="landing__bottom_container">
+        <span className="landing__heading">If not now, then when?</span>
+        <span className="landing__title">
+          Time to make your wealth work for you
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
