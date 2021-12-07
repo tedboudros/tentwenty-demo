e9c09eb3c239caed77baef6e804b2c95839af523
Ted Boudros <theodore.boudros.design@gmail.com>
2021-12-08 00:30:50 +0200

    [chore] Move everything to the without-cra folder and make a with-cra folder that has create-react-app

---
+++ without-cra/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Hello tentSwenty</div>;
+};
+
+export default App;
---
+++ without-cra/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+};
