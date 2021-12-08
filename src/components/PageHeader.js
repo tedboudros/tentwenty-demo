4f723f7283409449ca0ed01f36c801f47f654e8b
Ted Boudros <theodore.boudros.design@gmail.com>
2021-12-08 21:09:18 +0200

    [feat] Add logo and links to header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Button from "./Button";
 
-const PageHeader = ({ logo, centerComponents, rightComponents }) => {
-  return <div></div>;
+const PageHeaderLinks = ({ config }) => {
+  return (
+    <div className="page-header__links">
+      {(config || []).map((link, index) => (
+        <a>{link.text}</a>
+      ))}
+    </div>
+  );
+};
+
+const PageHeader = ({ leftSideConfig, rightSideConfig }) => {
+  return (
+    <div className="page-header">
+      <div className="page-header__left">
+        <div className="page-header__left__logo" />
+        <PageHeaderLinks config={leftSideConfig} />
+      </div>
+      <div className="page-header__right">
+        <PageHeaderLinks config={rightSideConfig} />
+        <Button text="Book A meeting" />
+      </div>
+    </div>
+  );
 };
 
 export default PageHeader;
--- src/view/index.js
+++ src/view/index.js
@@ -1,10 +1,28 @@
+import PageHeader from "../components/PageHeader";
 import React from "react";
 
 import Landing from "./sections/landing";
 
 const Home = () => {
+  // the urls don't lead anywhere :(
+  const headerLeftSideConfig = [
+    { text: "Private clients", url: "/private-clients" },
+    { text: "Corporates", url: "/corporates" },
+    { text: "Insights", url: "/insights" },
+    { text: "Contact", url: "/contact" },
+  ];
+
+  const headerRightSideConfig = [
+    { text: "About", url: "/about" },
+    { text: "Careers", url: "/careers" },
+  ];
+
   return (
-    <div>
+    <div className="home-wrapper">
+      <PageHeader
+        leftSideConfig={headerLeftSideConfig}
+        rightSideConfig={headerRightSideConfig}
+      />
       <Landing />
     </div>
   );
